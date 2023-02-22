6e3106089681ccc5046af3bbf2786edb16eee66a
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-02-22 16:39:19 +0800

    Registration and login done

--- api/index.js
+++ api/index.js
@@ -1,13 +1,57 @@
-// $ yarn add express
+const express = require("express");
+var cors = require("cors");
+const mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+const User = require("./models/User");
+require("dotenv").config();
 
-const express = require('express');
 const app = express();
-const port = 5000
+const port = 5000;
+const beryptSalt = bcrypt.genSaltSync(10);
 
-app.get('/', (req, res) => {
-    res.json('Hello world');
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("Hello world");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, beryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    res.json("found");
+  } else {
+    res.json("not found");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`);
+});
+
+//   booking
+//   SwqF2h3bb04PNLiH
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const RegisterPage = () => {
+  const [name, setname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+   const  registerUser = async (ev) => {
+    ev.preventDefault();
+    // console.log(ev.target.value);
+    try {
+      await axios.post('/register', {
+        name,
+        email,
+        password
+      });
+      alert('Registration successful. Now you can login.')
+    } catch(e) {
+      alert('Registration fails. Please try again later.')
+    }
+    
+  }
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="-mt-16">
         <h1 className="text-4xl text-center mb-4 font-bold">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="primary">Login</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(ev) => setname(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          <button className="primary">Register</button>
 
-          <div className="text-center py-2 text-gray-500">Allready a member <Link className="underline text-black" to={'/login'}>Login</Link></div>
+          <div className="text-center py-2 text-gray-500">
+            Allready a member{" "}
+            <Link className="underline text-black" to={"/login"}>
+              Login
+            </Link>
+          </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
