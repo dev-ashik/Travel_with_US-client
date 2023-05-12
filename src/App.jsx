a115670d1d384e021f0873dd5e6a42f38367d5a0
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-05-12 10:01:01 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,8 @@ import { PlacePage } from "./pages/PlacePage";
 import { BookingsPage } from "./pages/BookingsPage";
 import { BookingPage } from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://127.0.0.1:5000";
+// axios.defaults.baseURL = "http://127.0.0.1:5000";
+axios.defaults.baseURL = "https://travel-with-us.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
--- src/Port.js
+++ src/Port.js
@@ -1,1 +1,2 @@
-export const serverPort = 'https://travel-with-us.onrender.com';
+// export const serverPort = 'https://travel-with-us.onrender.com';
+export const serverPort = 'http://127.0.0.1:5000';
